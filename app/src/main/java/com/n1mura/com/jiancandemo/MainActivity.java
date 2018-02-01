1d44fdf1931e9b1b99ccd057e788f0ae68aff26e
VamZero <308170166@qq.com>
2018-02-01 23:43:31 +0800

    Initial commit

---
+++ app/src/main/java/com/n1mura/com/jiancandemo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.n1mura.com.jiancandemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.TextureMapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextureMapView mMapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapInit(savedInstanceState);
+    }
+
+    protected void mapInit(Bundle savedInstanceState) {
+
+        //获取地图控件引用
+        mMapView = (TextureMapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+
+        //初始化地图控制器对象
+        AMap aMap = null;
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+}
